9e1cfca78b2597429dfba5aa86190e51862e88b3
terylla <72099370+terylla@users.noreply.github.com>
2020-11-03 14:53:42 -0500

    update

--- November/Maximum Consecutive Character/maxPower.java
+++ November/Maximum Consecutive Character/maxPower.java
@@ -3,25 +3,25 @@ class Solution {
         int count = 1;
         int totalCount = 0;
         int length = s.length();
-        
+
         if (s.length() == 0) {
             return 0;
         }
         if (s.length() == 1){
             return 1;
         }
-        
+
         if (s.length() > 1){
             for (int i = 0; i < length; i++){
                 if (i < length -1 && s.charAt(i) == (s.charAt(i+1))){
                     count++;
-                } else {        
-                      count = 1;  
-                  }    
-                
-                   if (totalCount < count){
-                        totalCount= count;             
-                } 
+                } else {
+                    count = 1;
+                }
+
+                if (totalCount < count){
+                    totalCount= count;
+                }
             }
         }
         return totalCount;
