62aec0e720fc5ce65c49a29bd5f26797ffc2c351
TL <72099370+terylla@users.noreply.github.com>
2021-01-02 18:21:23 -0500

    Update Merge Two Sorted List .java

--- Problems Solved/Merge Two Sorted List .java
+++ Problems Solved/Merge Two Sorted List .java
@@ -23,9 +23,18 @@ Input: l1 = [], l2 = [0]
 Output: [0]
 */
 
-*     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
    
